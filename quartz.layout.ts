0c4cfe3502342598fd16c5831f5f794cb8bd15bf
khy07181 <khy07181@gmail.com>
2025-02-23 11:10:27 +0900

    Quartz sync: Feb 23, 2025, 11:10 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,11 +5,19 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        repo: 'khy07181/blog',
+        repoId: 'R_kgDON9-dEA',
+        category: 'Announcements',
+        categoryId: 'DIC_kwDON9-dEM4CnOik',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
