5aaa0277c4f2dbd28f02d8b4895dde4b69f9804d
khy07181 <khy07181@gmail.com>
2025-02-24 18:18:42 +0900

    Quartz sync: Feb 24, 2025, 6:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
