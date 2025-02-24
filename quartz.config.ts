a344ad313e3f1fa7040af0976ad79799f3785e29
khy07181 <khy07181@gmail.com>
2025-02-24 18:24:49 +0900

    Quartz sync: Feb 24, 2025, 6:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,6 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: {
       provider: "google",
+      tagId: "G-5LV1MFRSYJ",
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
